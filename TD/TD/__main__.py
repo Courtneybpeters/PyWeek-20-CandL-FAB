593b03aac5501a00ebd844661b1cae7fa5d709e8
Luke Paireepinart <lukepdev@live.com>
2015-08-10 23:28:08 -0500

    - added a solution function to take the map and calculate the path from the start to the end nodes - added a demo of a unit moving along the path.

--- TD/TD/__main__.py
+++ TD/TD/__main__.py
@@ -36,7 +36,35 @@ class Map (object):
                 temp.append(int(char))
             self.mapdata.append(temp)
     def get(self, x, y):
-        return TILETYPE[self.mapdata[x][y]]
+        return TILETYPE[self.mapdata[y][x]]
+
+    def findpath(self):
+        # determine where the start and end path are and then find all
+        # consecutive paths between the start and end.
+        start = end = None
+        for x in range(BOARD_WIDTH):
+            for y in range(BOARD_HEIGHT):
+                if self.get(x,y) == "pathstart":
+                    start = (x,y)
+                elif self.get(x,y) == "pathend":
+                    end = (x,y)
+        if (start != None and end != None):
+            # find path from start to end.
+            current = start
+            solution = []
+            while current != end:
+                solution.append(current)
+                #find a cell up, down, left or right of our current that's not in used_paths.
+                testcells = [(current[0]-1, current[1]), (current[0],current[1]-1), (current[0]+1, current[1]), (current[0], current[1]+1)]
+                for cell in testcells:
+                    if cell not in solution and 0 < cell[0] < BOARD_WIDTH and 0 < cell[1] < BOARD_HEIGHT and self.get(cell[0], cell[1]) in ["pathstart", "pathend", "path"]:
+                        current = cell
+                        break
+            solution.append(end)
+        print solution
+        return solution
+
+
 class Game (object):
     def __init__(self):
         self.state = "mainmenu"
@@ -60,7 +88,7 @@ class Game (object):
         for x in range(BOARD_WIDTH):
             for y in range(BOARD_HEIGHT):
                 #draw map
-                cell = self.map.get(y,x)
+                cell = self.map.get(x,y)
                 fillcolor = BGCOLOR
                 if (cell == "path"):
                     fillcolor = PATHCOLOR
@@ -86,15 +114,22 @@ def main():
     # initialize pygame window
     game = Game()
     game.load_level("map1")
+    path = game.map.findpath()
     #skip "mainmenu" state since we're in development
     game.set_state("playing")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     #Main Loop
     exit = False
+    p = 0
     while not exit:
 
         #update screen
         game.draw(screen)
+        #this is just a test to see a unit move along the path.
+        p += .0015
+        p %= len(path)
+        p2 = int(p)
+        screen.fill((0,0,0), (path[p2][0]*CELL_SIZE, path[p2][1]*CELL_SIZE, CELL_SIZE, CELL_SIZE))
         pygame.display.update()
 
         # event loop for user input
